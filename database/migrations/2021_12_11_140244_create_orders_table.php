4304e49cc2452dd1284b80eaa94c133d5a39aa35
JohnNajm <jmnajm@ndu.edu.lb>
2021-12-11 17:36:11 +0200

    you can now place an order

---
+++ database/migrations/2021_12_11_140244_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onupdate('cascade')->onDelete('set null');
+            $table->string('billing_name');
+            $table->string('billing_email');
+            $table->string('billing_address');
+            $table->string('billing_phone');
+            $table->string('billing_total');
+            $table->boolean('shipped');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -12,7 +12,7 @@
         <div class="col-md-12">
           <div class="full">
             <div class="title-holder">
-              <div class="title-holder-cell text-left">
+              <div class="text-left title-holder-cell">
                 <h1 class="page-title">Checkout</h1>
                 <ol class="breadcrumb">
                   <li><a href="/">Home</a></li>
@@ -32,8 +32,8 @@
 
     <div class="container">
       <div class="row">
-        <div class="col-md-4 order-md-2 mb-4">
-          <h4 class="d-flex justify-content-between align-items-center mb-3">
+        <div class="mb-4 col-md-4 order-md-2">
+          <h4 class="mb-3 d-flex justify-content-between align-items-center">
             <span class="text-muted">Your cart</span>
 
             <span class="cart-count">
@@ -41,7 +41,7 @@
                 <span class="badge badge-secondary badge-pill">{{ Cart::instance('default')->count() }}</span></span>
                 @endif
           </h4>
-          <ul class="list-group mb-3">
+          <ul class="mb-3 list-group">
             <li class="list-group-item d-flex justify-content-between lh-condensed">
                 <div>
                     <h6 class="my-0">Product</h6>
@@ -66,7 +66,7 @@
             </li>
           </ul>
 
-          <form class="card p-2" action="" >
+          <form class="p-2 card" action="" >
             <div class="input-group">
               <input type="text" class="form-control" placeholder="Promo code">
               <div class="input-group-append">
@@ -80,19 +80,20 @@
           <form class="needs-validation" novalidate action={{ route('checkout.store') }} >
             {{ csrf_field() }}
             <div class="row">
-              <div class="col-md-6 mb-3">
+              <div class="mb-3 col-md-6">
                 <input type="text" class="form-control" id="firstName" placeholder="{{ auth()->user()->name }}" value="" required>
-                <input type="hidden" name="u_id" value="{{ auth()->user()->id }}">
+                {{-- <input type="hidden" name="u_id" value="{{ auth()->user()->id }}">
                 <input type="hidden" name="u_email" value="{{ auth()->user()->email }}">
                 <input type="hidden" name="u_Address" value="{{ auth()->user()->Address }}">
                 <input type="hidden" name="u_name" value="{{ auth()->user()->name }}">
+                <input type="hidden" name="u_phone" value="{{ auth()->user()->phonenumber }}"> --}}
                 <input type="hidden" name="c_total" value="{{ Cart::total() }}">
               </div>
             </div>
             <hr class="mb-4">
             <h4 class="mb-3">Payment</h4>
             <div class="row">
-              <div class="col-md-6 mb-3">
+              <div class="mb-3 col-md-6">
                 <label for="cc-name">Name on card</label>
                 <input type="text" class="form-control" id="cc-name" placeholder="" required>
                 <small class="text-muted">Full name as displayed on card</small>
@@ -100,7 +101,7 @@
                   Name on card is required
                 </div>
               </div>
-              <div class="col-md-6 mb-3">
+              <div class="mb-3 col-md-6">
                 <label for="cc-number">Credit card number</label>
                 <input type="text" class="form-control" id="cc-number" placeholder="" required>
                 <div class="invalid-feedback">
@@ -109,14 +110,14 @@
               </div>
             </div>
             <div class="row">
-              <div class="col-md-3 mb-3">
+              <div class="mb-3 col-md-3">
                 <label for="cc-expiration">Expiration</label>
                 <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
                 <div class="invalid-feedback">
                   Expiration date required
                 </div>
               </div>
-              <div class="col-md-3 mb-3">
+              <div class="mb-3 col-md-3">
                 <label for="cc-expiration">CVV</label>
                 <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
                 <div class="invalid-feedback">
